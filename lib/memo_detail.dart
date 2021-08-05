188b2f986c3266310108030de91dd4f3f75591b9
KeitaFukue <k.fukue.it@gmail.com>
2021-08-05 18:19:10 +0900

    TextFieldでテキスト入力 maxlinesとminlinesをうまく使うと，入力領域の大きさを調整できる

--- lib/memo_detail.dart
+++ lib/memo_detail.dart
@@ -4,9 +4,19 @@ class MemoDetail extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'メモ入力しとけって'
+        ),
+      ),
       body: Center(
-        child: Text(
-        'oh yeah',
+        child: TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Write something',
+          ),
+          maxLines: 3,
+          minLines: 3,
         ),
       ),
     );
